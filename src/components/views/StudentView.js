7c0b70a370c08e62ee38ec032f4ddcd3aa73416c
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 15:37:27 -0500

    can navigate to campus from student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const StudentView = (props) => {
   const { student } = props;
 
   // Ternary operator to decide whether to display school or helpful message
   let enrollment = (student.campus
-    ? (<h3>Enrolled at {student.campus.name}</h3>)
+    ? (<h3>Enrolled at <Link to={`/campus/${student.campus.id}`}>{student.campus.name}</Link></h3>)
     : (<h3>Not enrolled at any campus</h3>))
 
   return (
