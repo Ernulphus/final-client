cccd1df3f1a05d06fe61724f5dbf1a738a39b26d
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-12-18 15:04:28 -0500

    student view displays message if not enrolled

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,11 +1,19 @@
 const StudentView = (props) => {
   const { student } = props;
+  if (student.campus)
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <h3>Enrolled at {student.campus.name}</h3>
     </div>
   );
+  else
+  return (
+    <div>
+      <h1>{student.firstname + " " + student.lastname}</h1>
+      <h3>Not enrolled at any campus</h3>
+    </div>
+  )
 
 };
 
